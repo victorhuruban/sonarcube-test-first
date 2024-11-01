01069ebc9326bb5c736503fdc073c7388770cf7d
Victor Huruban <victor.huruban@yahoo.com>
2024-11-01 20:29:34 +0000

    First commit

---
+++ src/main/kotlin/com/sonar/cube/test/first/sonarcube_test_first/SonarcubeTestFirstApplication.kt
@@ -0,0 +1,11 @@
+package com.sonar.cube.test.first.sonarcube_test_first
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SonarcubeTestFirstApplication
+
+fun main(args: Array<String>) {
+	runApplication<SonarcubeTestFirstApplication>(*args)
+}
---
+++ src/test/kotlin/com/sonar/cube/test/first/sonarcube_test_first/SonarcubeTestFirstApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sonar.cube.test.first.sonarcube_test_first
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SonarcubeTestFirstApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
